a2f3df690192cb85fb7aacd53b098d6c0072f7d1
Hans Hsieh <539874+hanshsieh@users.noreply.github.com>
2021-03-06 16:53:46 +0000

    Migrate to support Java 9 module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.github.hanshsieh.pixivj {
+  requires org.checkerframework.checker.qual;
+  requires org.slf4j;
+  requires org.apache.commons.lang3;
+  requires kotlin.stdlib;
+  requires kotlin.stdlib.common;
+  requires okhttp3;
+  requires com.google.gson;
+  exports com.github.hanshsieh.pixivj.token;
+  exports com.github.hanshsieh.pixivj.api;
+  exports com.github.hanshsieh.pixivj.oauth;
+  exports com.github.hanshsieh.pixivj.model;
+  exports com.github.hanshsieh.pixivj.exception;
+}
