6f4b51d34ba46a7453a13e8639658dc0a8fb4bc2
Hans Hsieh <539874+hanshsieh@users.noreply.github.com>
2021-03-20 05:17:54 +0100

    Add unit tests for PixivApiClient
    - Add unit tests for PixivApiClient
    - Rename `SearchIllustsFilter` to `SearchedIllustsFilter`.

--- src/main/java/com/github/hanshsieh/pixivj/util/IoUtils.java
+++ src/main/java/com/github/hanshsieh/pixivj/util/IoUtils.java
@@ -4,6 +4,12 @@ import okhttp3.OkHttpClient;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class IoUtils {
+
+  /**
+   * Closes the given {@link OkHttpClient} and releases the resources.
+   * Active connection and requests won't be affected.
+   * @param httpClient HTTP client to be closed.
+   */
   public static void close(@NonNull OkHttpClient httpClient) {
     httpClient.dispatcher().executorService().shutdown();
     httpClient.connectionPool().evictAll();
--- src/main/java/com/github/hanshsieh/pixivj/util/QueryParamConverter.java
+++ src/main/java/com/github/hanshsieh/pixivj/util/QueryParamConverter.java
@@ -6,8 +6,18 @@ import java.util.Map;
 import okhttp3.HttpUrl;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+/**
+ * This converts the properties of an object to URL query parameters.
+ */
 public class QueryParamConverter {
 
+  /**
+   * Converts the properties of {@code obj} to query parameters, and add the query parameters to
+   * the given {@code urlBuilder}. The values of the properties will be serialized as JSON strings.
+   * @param obj The object to provide the query parameters values.
+   * @param urlBuilder The URL builder to add the generated query parameters.
+   * @param <T> The type of the object.
+   */
   public <T> void toQueryParams(@NonNull T obj, HttpUrl.@NonNull Builder urlBuilder) {
     JsonObject jsonObj = JsonUtils.GSON.toJsonTree(obj)
         .getAsJsonObject();
